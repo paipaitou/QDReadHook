4bf5cf65214746f2f43e0ea9ca44043605bbc373
paipaitou <pangyongfeng@gmail.com>
2024-05-20 12:49:01 +0800

    1

--- app/src/main/kotlin/cn/xihan/qdds/Option.kt
+++ app/src/main/kotlin/cn/xihan/qdds/Option.kt
@@ -116,7 +116,7 @@ object Option {
      * @suppress Generate Documentation
      */
     val basePath =
-        "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)}/QDHook/QDReader/"
+        "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)}/QDHook/QDReader/" + this::class.qualifiedName
 
     /**
      * 重定向主题路径
