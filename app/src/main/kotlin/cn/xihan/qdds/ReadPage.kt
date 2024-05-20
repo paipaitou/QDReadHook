2c0af6a4cbd04b2f2b36328bfd95650ba12a48a2
paipaitou <pangyongfeng@gmail.com>
2024-05-20 12:08:38 +0800

    1

--- app/src/main/kotlin/cn/xihan/qdds/ReadPage.kt
+++ app/src/main/kotlin/cn/xihan/qdds/ReadPage.kt
@@ -323,9 +323,9 @@ fun PackageParam.readingTimeSpeedFactor(
                                 val currentTime = System.currentTimeMillis()
                                 val bookType = it.getParam<Int>("bookType")
                                 val option = optionEntity.readPageOption
-                                var startTime2 = 0
+                                var startTime2: Long = 0L
                                 var totalTime2 = ((totalTime ?: 10000) * speedFactor)
-                                var endTime2 = 0
+                                var endTime2: Long = 0L
                                 if ( bookType == 1) {
                                     
                                     if ( (currentTime + 8 *3600)/3600/24 - (option.lastTime1 + 8 *3600)/3600/24 > 0 ){                                        
